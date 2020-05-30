b1d2a576911bf4fc7631aebb59c0efde927457e2
ManojDjs <djsmanoj0000@gmail.com>
2020-05-30 14:54:22 +0100

    loading index page with icons

--- sneaky/groupapp/urls.py
+++ sneaky/groupapp/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-from . import views
+from django.urls import path, re_path
+from .views import chat, room
+
+
+app_name = 'groupapp'
 
 urlpatterns = [
-    path('', views.chat, name='chat'),
-    path('<str:room_name>/', views.room, name='room'),
-
+    path('', chat, name='chat'),
+    re_path(r'^(?P<room_name>[^/]+)/$', room, name='room'),
 ]
--- sneaky/sneaky/routing.py
+++ sneaky/sneaky/routing.py
@@ -1,10 +1,8 @@
-# mysite/routing.py
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import groupapp.routing
 
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter(
             groupapp.routing.websocket_urlpatterns
